f13047facf55169ed59a5857bddba9f02dc39bd3
oybek777 <153380033+oybekvip777@users.noreply.github.com>
2023-12-19 19:38:26 +0500

    Add files via upload

---
+++ record1.9.py
@@ -0,0 +1,63 @@
+# 1-xato
+
+# print "Hello World!"
+
+# 2-xato
+
+# print("Hello World!"
+
+# 3-xato
+
+# print("Hello World!
+
+# 4-xato
+
+# print("O'ngacha sanaymiz")
+# for n in range(10):
+# print(n+1)
+
+# 5-xato
+
+# son = input("Istalgan son kiriting: ")
+# print(f"{son} ning kvadrati {son**2} ga teng")
+
+# 6-xato
+
+# mevalar = ['olma','uzum','nok','anor','anjir']
+# for meva in mvealar:
+    # print(meva)
+
+# 7-xato
+
+# son = int(input("Istalgan son kiriting: "))
+# if son>=0:
+#     print("Musbat son")
+# else:
+#     print("Manfiy son")
+
+# 8-xato
+
+# mevalar = ['olma','anor','uzum']
+# print(mevalar[3])
+
+# 9-xato
+
+# x, y = 50, 50
+# z = 250/(x-y)
+
+# 10-xato
+
+# radius = 5
+# pi = 4.14
+# aylana_yuzi = pi*radius**2
+# print(aylana_yuzi)
+
+
+
+
+
+
+
+
+
+
